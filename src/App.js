c75c54330e4c9ff874118fe04533fdddbc9cd46e
tc89585 <tc89585@uga.edu>
2022-06-20 19:15:20 -0400

    complete feature

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import NavbarComp from './component/navigation/NavbarComp';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" data-testid="app-component">
       <NavbarComp/>
     </div>
   );
